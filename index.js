06c919f181b04bc3783e4ad31b7d8a44934e4a87
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-10-16 12:01:12 +0300

    form 'add course' added

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 app.use('/', RouterHome)
 app.use('/courses', RouterCourses)
 app.use('/add', RouterAdd)
-
-
-
 
 const PORT = process.env.PORT || 3000
 
