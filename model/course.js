4d4eb1a9445e76579788dd1030957f317eeedca5
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-10-28 14:13:10 +0300

    course window is done

--- model/course.js
+++ model/course.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const fsPromises = require('fs/promises');
 const path = require("path");
 
@@ -11,16 +11,21 @@ class Course {
     }
 
     static getAllCourses() {
-        return new Promise((resolve, reject)=>{
-            (async (path) =>{
+        return new Promise((resolve, reject) => {
+            (async (path) => {
                 try {
                     let data = await fsPromises.readFile(path, "utf-8")
                     resolve(JSON.parse(data))
                 } catch (err) {
                     reject(err)
                 }
-            })(path.join(__dirname, '..','data', 'courses.json'))
+            })(path.join(__dirname, '..', 'data', 'courses.json'))
         })
+    }
+
+    static async getByIdCourse(id) {
+        const courses = await Course.getAllCourses()
+        return courses.find(c => c.id === id)
     }
 
     getCurrentCourse() {
@@ -41,10 +46,10 @@ class Course {
 
             return new Promise((resolve, reject) => {
                 try {
-                    (async (path) =>{
+                    (async (path) => {
                         await fsPromises.writeFile(path, JSON.stringify(courses))
                         resolve()
-                    })(path.join(__dirname, '..','data', 'courses.json'))
+                    })(path.join(__dirname, '..', 'data', 'courses.json'))
                 } catch (e) {
                     reject(e)
                 }
--- routes/courses.js
+++ routes/courses.js
@@ -6,9 +6,18 @@ router.get('/', async (req, res) => {
 
     const courses = await Courses.getAllCourses()
     res.render('courses', {
-        title: 'Courses',
+        title: `Courses ${courses.length}`,
         isCourses: true,
         courses,
+    })
+})
+
+router.get('/:id', async (req, res) => {
+
+    const course = await Courses.getByIdCourse(req.params.id)
+    res.render('course', {
+        title: `Course ${course.title}`,
+        course
     })
 })
 
