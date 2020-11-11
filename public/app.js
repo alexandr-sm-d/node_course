9fd28761f9335b4c324600f74859b84e50a5cb21
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-11-11 12:11:04 +0300

    added dynamic render js

--- public/app.js
+++ public/app.js
@@ -1,9 +1,13 @@
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru', {
+        style: 'currency',
+        currency: 'RUB'
+    }).format(+price)
+}
+
 document.querySelectorAll('.price')
     .forEach(node => {
-        node.textContent = new Intl.NumberFormat('ru', {
-            style: 'currency',
-            currency: 'RUB'
-        }).format(+node.textContent)
+        node.textContent = toCurrency(node.textContent)
     })
 
 
@@ -11,7 +15,6 @@ let $cart = document.querySelector('#cart')
 if ($cart) {
     $cart.addEventListener('click', event => {
         if (event.target.classList.contains('js-remove')) {
-            // console.log('button delete')
             const id = event.target.dataset.id
             fetch('/cart/remove/' + id, {
                 method: "delete"
@@ -19,6 +22,21 @@ if ($cart) {
                 .then(res => res.json())
                 .then(data => {
                     console.log(data)
+                    if (data.products.length) {
+                        let tr = data.products.map(p => `
+                            <tr>
+                                <td>${p.title}</td>
+                                <td>${p.count}</td>
+                                <td>
+                                    <button class="btn-small js-remove" data-id="${p.id}">Delete</button>
+                                </td>
+                            </tr>
+                        `).join('')
+                        $cart.querySelector('tbody').innerHTML = tr
+                        $cart.querySelector('.price').textContent = toCurrency(data.totalPrice)
+                    } else {
+                        $cart.innerHTML = '<h2>Cart is empty</h2>'
+                    }
                 })
         }
     })
