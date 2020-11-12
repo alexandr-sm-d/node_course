bca22688539a9983a1f77361cfcb0a8d197f9d9a
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-11-12 10:10:24 +0300

    created user model

---
+++ model/user.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require('mongoose')
+
+const user = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                count: {
+                    type: Number,
+                    required: true,
+                    default: 1
+                },
+                courseID: {
+                    type: Schema.Types.ObjectID,
+                    required: true,
+                    ref: 'Courses'
+                }
+            }
+        ]
+    }
+})
+
+module.exports = model('User', user)
